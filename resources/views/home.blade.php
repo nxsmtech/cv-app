f8f0a517c02ea6cd70898f5321de07fa3531027a
nikita <nx.smtech@gmail.com>
2023-12-17 14:40:36 +0200

    Add tailwind and a simple cv's index page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,40 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV's List</title>
+    @vite('resources/css/app.css')
 </head>
-<body>
-    @foreach($cvs as $cv)
-        <p> {{ $cv->name }} </p>
-    @endforeach
+<body class="bg-gray-100">
+<header class="bg-white shadow">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold text-gray-900">CV's</h1>
+    </div>
+    <div class="flex justify-end px-4 sm:px-6 lg:px-8">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Add CV
+        </button>
+    </div>
+</header>
+
+<main class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+    <!-- Replace with your content -->
+    <div class="px-4 py-6 sm:px-0">
+        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96">
+            @for ($i = 0; $i < 5; $i++)
+                <div class="flex items-center justify-between p-3 border-b border-gray-300">
+                    <span class="text-lg font-medium text-gray-700">Random Name {{$i + 1}}</span>
+                    <div>
+                        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-2">Open</button>
+                        <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded ml-2">Edit</button>
+                        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2">Delete</button>
+                    </div>
+                </div>
+            @endfor
+        </div>
+    </div>
+    <!-- /End replace -->
+</main>
 </body>
 </html>
